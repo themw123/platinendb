bbf5dcc2a6c08db8cb9be7c2c89a82d5e6f43c94
themw123 <80266862+themw123@users.noreply.github.com>
2021-09-08 10:32:30 +0200

    Register bestätigen5

--- views/register.php
+++ views/register.php
@@ -25,8 +25,60 @@ if ist da, damit einfliegende registerform nicht erneut angezeigt wird, wenn reg
 
 if ($registration->messages == null ) {
 
-    if(!isset($_GET["Validation"])) {
+    if(isset($_GET["Validation"]) && isset($_GET["user_name"]) && isset($_GET["user_email"]) && isset($_GET["user_password"]) && isset($_GET["user_standort"])) {
+        echo
+        '<div class="wrapper fadeInDown">
+
+        <div id="formContent">
+
+        <div class="fadeIn first"> 
+        
+        <i class="fas fa-user-alt" id = icon1></i>
     
+        </div> 
+        
+
+        <form method="post" action="registerindex.php" name="registerform">
+    
+        <input value='.$_GET["user_name"].' id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" placeholder="Benutzername" required >
+    
+        <input value='.$_GET["user_email"].' id="login_input_email" class="login_input" type="email" name="user_email" placeholder="E-Mail-Adresse" required /> 
+
+        <input value='.$_GET["user_password"].' id="login_input_password_new" class="login_input" type="hidden" name="user_password" pattern=".{6,}" placeholder="Passwort (min. 6 Zeichen)"  required autocomplete="off" />
+    
+    
+        <div class="containerintext">
+        <p>Standort?</p>
+        <label class="radio-inline" id="option1">
+
+        ';
+        if($_GET["user_standort"] == "int") {
+            echo'<input value="int" type="radio" name="user_standort" checked>intern';
+        }
+        else {
+            echo'<input value="int" type="radio" name="user_standort">intern';
+        }
+        echo'
+        </label>
+        <label class="radio-inline" id="option2">
+        ';
+        if($_GET["user_standort"] == "ext") {
+            echo'<input value="ext" type="radio" name="user_standort" checked>extern';
+        }
+        else {
+            echo'<input value="ext" type="radio" name="user_standort">extern';
+        }
+        echo'
+        </label>
+        </div>
+
+        
+        <input id="registrierensubmit" type="submit" name="reg" class="fadeIn second" value="anlegen/bestätigen">
+
+        ';
+    }
+
+    else {
         echo
 
         '<div class="wrapper fadeInDown">
@@ -65,72 +117,15 @@ if ($registration->messages == null ) {
 
         ';
     }
-    else {
-
-        if(isset($_GET["user_name"]) && isset($_GET["user_email"]) && isset($_GET["user_password"]) && isset($_GET["user_standort"])) {
-            echo
-            '<div class="wrapper fadeInDown">
-
-            <div id="formContent">
-
-            <div class="fadeIn first"> 
-            
-            <i class="fas fa-user-alt" id = icon1></i>
-        
-            </div> 
-            
-
-            <form method="post" action="registerindex.php" name="registerform">
-        
-            <input value='.$_GET["user_name"].' id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" placeholder="Benutzername" required >
-        
-            <input value='.$_GET["user_email"].' id="login_input_email" class="login_input" type="email" name="user_email" placeholder="E-Mail-Adresse" required /> 
-
-            <input value='.$_GET["user_password"].' id="login_input_password_new" class="login_input" type="hidden" name="user_password" pattern=".{6,}" placeholder="Passwort (min. 6 Zeichen)"  required autocomplete="off" />
-        
-        
-            <div class="containerintext">
-            <p>Standort?</p>
-            <label class="radio-inline" id="option1">
-
-            ';
-            if($_GET["user_standort"] == "int") {
-                echo'<input value="int" type="radio" name="user_standort" checked>intern';
-            }
-            else {
-                echo'<input value="int" type="radio" name="user_standort">intern';
-            }
-            echo'
-            </label>
-            <label class="radio-inline" id="option2">
-            ';
-            if($_GET["user_standort"] == "ext") {
-                echo'<input value="ext" type="radio" name="user_standort" checked>extern';
-            }
-            else {
-                echo'<input value="ext" type="radio" name="user_standort">extern';
-            }
-            echo'
-            </label>
-            </div>
-
-            
-            <input id="registrierensubmit" type="submit" name="reg" class="fadeIn second" value="anlegen/bestätigen">
-
-            ';
-        }
-    }
 
 
-
-    
 
 
     echo'
     <div id="formFooter">
     ';
 
-    if(isset($_GET["Validation"])) {
+    if(isset($_GET["Validation"]) && isset($_GET["user_name"]) && isset($_GET["user_email"]) && isset($_GET["user_password"]) && isset($_GET["user_standort"])) {
         echo '<a id="new-board-btn" class="fadeIn third" href="index.php">zurück zur Startseite</a>';
     }
     else {
