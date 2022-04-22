c32d1e04b01536c87a3cecb6f5e9af4b7492b525
themw123 <marvin.w6@web.de>
2022-04-22 12:55:44 +0200

    Signed-off-by: themw123 <marvin.w6@web.de>

--- verarbeitungPl/auftraggeber/addauftraggeber.php
+++ verarbeitungPl/auftraggeber/addauftraggeber.php
@@ -26,7 +26,7 @@ if($bestanden == true) {
       $auftraggeber = mysqli_real_escape_string($platinendb_connection, $_POST['bearOderAuftr']);
 
 
-      $add = "INSERT INTO users(user_name) VALUE('$auftraggeber')";
+      $add = "INSERT INTO users(user_name, admin) VALUE('$auftraggeber', '0')";
 
 
       mysqli_query($login_connection, $add);
