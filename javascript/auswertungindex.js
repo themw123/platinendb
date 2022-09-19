ecfa7e1a78a9ada622a21dc0c21b66c84eb3fa7b
themw123 <marvin.w6@web.de>
2022-09-19 17:38:01 +0200

    chart2

--- javascript/auswertungindex.js
+++ javascript/auswertungindex.js
@@ -2,10 +2,11 @@
 const ctx1 = document.getElementById('chart1').getContext('2d');
 const ctx2 = document.getElementById('chart2').getContext('2d');
 const ctx3 = document.getElementById('chart3').getContext('2d');
+const ctx4 = document.getElementById('chart4').getContext('2d');
 
 
 const chart1 = new Chart(ctx1, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
@@ -77,7 +78,7 @@ const chart2 = new Chart(ctx2, {
 
 
 const chart3 = new Chart(ctx3, {
-  type: 'bar',
+  type: 'doughnut',
   data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
@@ -103,10 +104,51 @@ const chart3 = new Chart(ctx3, {
       }]
   },
   options: {
+    /*
       scales: {
           y: {
               beginAtZero: true
           }
       }
+    */
+  }
+});
+
+
+
+const chart4 = new Chart(ctx4, {
+  type: 'pie',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+    /*
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+    */
   }
 });
--- views/auswertung.php
+++ views/auswertung.php
@@ -11,6 +11,7 @@ echo'
 		<canvas class="chart" id="chart1"></canvas>
 		<canvas class="chart" id="chart2"></canvas>
 		<canvas class="chart" id="chart3"></canvas>
+		<canvas class="chart" id="chart4"></canvas>
 	</div>
 ';
 
