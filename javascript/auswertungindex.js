2228fb7dbf9202204cef78eec7c0a61da80f9c74
themw123 <marvin.w6@web.de>
2022-10-11 16:42:06 +0200

    n

--- javascript/auswertungindex.js
+++ javascript/auswertungindex.js
@@ -1,160 +1,136 @@
-
-
 setDefaultSettings();
 
 getData();
 
-
-
-
-$("#zeitinterval").on("input", function() {
-    setSettings1();
+$("#zeitinterval").on("input", function () {
+  setSettings1();
 });
 
-$("#jahroderletzten").on("input", function() {
-    setSettings2();
+$("#jahroderletzten").on("input", function () {
+  setSettings2();
 });
-
-
 
 function setDefaultSettings() {
   aktion = "auswertung";
   zeitraum = "monate";
 
   letzten = "x";
-  
 
   setJahre();
 
-
-
   datar = null;
-
-
 }
 
 function setSettings1() {
+  var jahroderletzten = $("#jahrlabel").text();
 
-  var jahroderletzten = $('#jahrlabel').text();
-  
   zeitraum = $("#zeitinterval option:selected").val().toLowerCase();
   jahr = $("#jahroderletzten option:selected").val();
   letzten = $("#jahroderletzten option:selected").val();
 
-
   if (zeitraum == "jahre" && jahroderletzten == "Jahr:") {
-   
-    $('#jahrlabel').text("Letzten:");
-
+    $("#jahrlabel").text("Letzten:");
 
     setLetzten();
 
-    $("#jahroderletzten option:last").attr("selected", "selected");  
-
-  }
-
-  else if (zeitraum == "monate" && jahroderletzten == "Letzten:") {
-   
-    $('#jahrlabel').text("Jahr:");
+    $("#jahroderletzten option:last").attr("selected", "selected");
+  } else if (zeitraum == "monate" && jahroderletzten == "Letzten:") {
+    $("#jahrlabel").text("Jahr:");
     setJahre();
-
   }
-
 
   setSettings2();
-
 }
-
 
 function setLetzten() {
   var $el = $("#jahroderletzten");
   $el.empty();
-  for(var i = 1; i <= 5; i++) {
+  /*
+  for (var i = 1; i <= 5; i++) {
     $el.append($("<option></option>").attr("value", i).text(i));
   }
-  $el.append($("<option></option>").attr("value", 0).text("alle"));
-
+  */
+  $el.append($("<option></option>").attr("value", 1).text(1));
+  $el.append($("<option></option>").attr("value", 3).text(3));
+  $el.append($("<option></option>").attr("value", 5).text(5));
+  $el.append($("<option></option>").attr("value", 10).text(10));
 }
 
 function setJahre() {
-  $('#jahroderletzten').empty();
+  $("#jahroderletzten").empty();
   jahr = new Date().getFullYear();
   for (var i = 0; i <= 10; i++) {
-    $('#jahroderletzten').append(new Option((jahr-i), (jahr-i)));
+    $("#jahroderletzten").append(new Option(jahr - i, jahr - i));
   }
 }
 
-
 function setSettings2() {
-
   zeitraum = $("#zeitinterval option:selected").val().toLowerCase();
   jahr = $("#jahroderletzten option:selected").val();
   letzten = $("#jahroderletzten option:selected").val();
 
-
   getData();
-
 }
 
-
-
-
-
-
 function getData() {
+  $.ajax({
+    url: "verarbeitungAus/auswertung.php",
+    method: "post",
+    data: { aktion: aktion, zeitraum: zeitraum, letzten: letzten, jahr: jahr },
+    success: function (data) {
+      try {
+        chart1.destroy();
+      } catch (error) { }
 
-  $.ajax({  
-                  url:"verarbeitungAus/auswertung.php",  
-                  method:"post",  
-                  data:{aktion:aktion, zeitraum:zeitraum, letzten:letzten, jahr:jahr},  
-                  success:function(data){
-                    
-                    try {
-                      chart1.destroy();
-                    }
-                    catch(error){
-                  
-                    }
+      var zustand = data.data[1];
 
+      if (zustand == "leer") {
+        $("#result")
+          .hide()
+          .fadeIn(1000)
+          .html(
+            '<div class="alert alert-warning alertm">Es wurden keine Daten gefunden</div>'
+          );
+      } else if (zustand == "fehlerhaft") {
+        $("#result")
+          .hide()
+          .fadeIn(1000)
+          .html(
+            '<div class="alert alert-danger alertm">Es ist ein Fehler im Zusammenhang mit der Sicherheit aufgetreten.</div>'
+          );
+      } else if (zustand == "dberror") {
+        $("#result")
+          .hide()
+          .fadeIn(1000)
+          .html(
+            '<div class="alert alert-danger alertm">Fehler bei der Durchführung des Datenbankbefehls. Fehler: ' +
+            data.data[2] +
+            "</div>"
+          );
+      }
 
-                    var zustand = data.data[1];
+      //erfolgreich
+      else {
+        datar = data.data;
 
-                    if (zustand == 'leer') {
-                      $('#result').hide().fadeIn(1000).html('<div class="alert alert-warning alertm">Es wurden keine Daten gefunden</div>');
-                    }
-                    else if(zustand == 'fehlerhaft'){
-                      $('#result').hide().fadeIn(1000).html('<div class="alert alert-danger alertm">Es ist ein Fehler im Zusammenhang mit der Sicherheit aufgetreten.</div>');
-                    }
-                    else if(zustand == 'dberror'){
-                      $('#result').hide().fadeIn(1000).html('<div class="alert alert-danger alertm">Fehler bei der Durchführung des Datenbankbefehls. Fehler: ' + data.data[2] +'</div>');
-                    }
+        if (zeitraum == "jahre") {
+          var datarTemp = new Array();
 
-                    //erfolgreich
-                    else {
+          if (letzten == 0) {
+            for (var i = datar.length; i >= 0; i--) {
+              datarTemp.push(datar[i]);
+            }
+          } else {
+            for (var i = letzten - 1; i >= 0; i--) {
+              datarTemp.push(datar[i]);
+            }
+          }
 
-                      datar = data.data;
+          datar = datarTemp;
+          datar = datar.filter((item) => item);
+        }
 
-                      if(zeitraum == "jahre") {
-                        var datarTemp = new Array;
-                        
-                        if(letzten == 0) {
-                          for(var i = datar.length; i >= 0; i--) {
-                            datarTemp.push(datar[i]);
-                          }
-                        }
-                        else {
-                          for(var i = letzten-1; i >= 0; i--) {
-                            datarTemp.push(datar[i]);
-                          }
-                        }
-                        
-
-
-                        datar = datarTemp;
-                        datar = datar.filter(item => item);
-                      }
-
-                      /*
+        /*
                       if(zeitraum == "monate") {
                         $.each(datar, function( key, value ){
                           $('#jahroderletzten').append('<option value="">' + value + '</option>');
@@ -162,77 +138,45 @@ function getData() {
                       }
                       */
 
-                      setChart();
+        setChart();
+      }
 
-                    }
-                        
-                    window.setTimeout(function() {
-                      $(".alertm").fadeTo(500, 0);
-                      $(this).remove();
-                    }, 5000);
-                  }  
-            }); 
-
- 
-
+      window.setTimeout(function () {
+        $(".alertm").fadeTo(500, 0);
+        $(this).remove();
+      }, 5000);
+    },
+  });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function setChart() {
-
-  ctx1 = document.getElementById('chart1').getContext('2d');
+  ctx1 = document.getElementById("chart1").getContext("2d");
   chart1 = new Chart(ctx1, {
+    type: "bar",
+    data: {
+      labels: getLabels(),
 
-      type: 'bar',
-      data: {
+      datasets: [
+        {
+          label: "# intern",
+          data: getValues("int"),
+          backgroundColor: ["rgba(46, 196, 87, 0.6)"],
+          borderColor: ["rgba(0, 0, 0, 0.6)"],
+          borderWidth: 1,
+        },
 
-          labels: getLabels(),
-          
-          datasets: [
-
-            {
-              label: '# intern',
-              data: getValues('int'),
-              backgroundColor: [
-                'rgba(46, 196, 87, 0.6)',
-              ],
-              borderColor: ['rgba(0, 0, 0, 0.6)'],
-              borderWidth: 1
-            },
-
-            {
-              label: '# extern',
-              data: getValues('ext'),
-              backgroundColor: [
-                'rgba(0, 172, 240, 0.6)',
-              ],
-              borderColor: ['rgba(0, 0, 0, 0.6)'],
-              borderWidth: 1
-            },
-
-
-            
-        ]
-      },
-      options: {
-
-
-          plugins: {
-            /*
+        {
+          label: "# extern",
+          data: getValues("ext"),
+          backgroundColor: ["rgba(0, 172, 240, 0.6)"],
+          borderColor: ["rgba(0, 0, 0, 0.6)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        /*
               title: {
                   display: true,
                   text: 'Platinenaufträge',
@@ -244,67 +188,44 @@ function setChart() {
                   }
               }
               */
-          },
+      },
 
-          scales: {
-              x: {
-                stacked: true,
-              },
-              y: {
-                stacked: true,
-                beginAtZero: true
-              }
-          },
-
-
-
-      }
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+          beginAtZero: true,
+        },
+      },
+    },
   });
 }
 
-
-
-
 function getLabels() {
-
-  var labels = new Array;
+  var labels = new Array();
 
   for (let i = 0; i < datar.length && i < 10; i++) {
-
-    labels.push(datar[i][0]+'');
-    
-  } 
-  
+    labels.push(datar[i][0] + "");
+  }
 
   return labels;
-
 }
 
-
 function getValues(intorext) {
-  
-  var dataArray = new Array;
+  var dataArray = new Array();
 
   for (let i = 0; i < datar.length; i++) {
     var stelle = 0;
-    if(intorext == "int") {
+    if (intorext == "int") {
       stelle = 2;
-    }
-    else {
+    } else {
       stelle = 3;
     }
 
     dataArray[i] = datar[i][stelle];
-  
-  } 
+  }
 
   return dataArray;
-
 }
-
-
-
-
-
-
-
